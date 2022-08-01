aac762083ab8d46addf24c96568c8af04c596832
anya.shan <anya.shanaida@yahoo.com>
2022-08-01 18:40:40 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listGalleryEl = document.querySelector('.gallery');
+
+const markupGalleryEl = markupItemsGalleryEl(galleryItems);
+listGalleryEl.insertAdjacentHTML('beforeend', markupGalleryEl);
+
+listGalleryEl.addEventListener('click', onImageClick);
+
+function markupItemsGalleryEl(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+    })
+    .join('');
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`);
+  instance.show();
+}
+
+// console.log(galleryItems);
